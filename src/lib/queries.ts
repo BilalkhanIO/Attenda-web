e3b9cb48d1c3182e502d677ad4a32ce7062aaa0e
Bilal Ahmad <bilalkhan301@gmail.com>
2026-06-16 19:16:40 +0500

    fixees

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -79,6 +79,13 @@ export interface MyTodayStatus {
     late_minutes?: number;
     net_hours_worked?: unknown;
     break_minutes?: number;
+    break_records?: {
+      break_type: string;
+      break_start: string;
+      break_end?: string | null;
+      duration_mins?: number | null;
+      is_paid?: boolean;
+    }[];
   } | null;
   pre_checkin_late_minutes?: number;
 }
